310d8d6584b1d83b7c695ed53091634bea12dba8
Greg Ford <greg.ford.2@googlemail.com>
2022-10-31 22:50:31 +1000

    Updates

--- arquery.py
+++ arquery.py
@@ -6,6 +6,9 @@ import urllib.parse
 import argparse
 from getpass import getpass
 import pprint
+import time
+import tempfile
+import json
 
 requests.packages.urllib3.disable_warnings()
 
@@ -105,12 +108,36 @@ class arquery:
         print(len(all_book_ids))
 
         for book_id in all_book_ids:
-            response = self.session.get(url="https://auhosted4.renlearn.com.au/1454662/AR/StudentApp/BookScore.aspx?i=" + book_id)
+            url = url="https://auhosted4.renlearn.com.au/1454662/AR/StudentApp/BookScore.aspx?i=" + book_id
+            # print(url)
+            response = self.session.get(url=url, verify=False, allow_redirects=False)
+            print(str(response.status_code) + " - " + str(len(response.text)) + " - " + url)
+            
+            # retry 5 times, with 5 second pauses
+            if response.status_code == 302:
+                for i in (1,5):
+                    time.sleep(5)
+                    response = self.session.get(url=url, verify=False, allow_redirects=False)
+                    print(str(response.status_code) + " - " + str(len(response.text)) + " - " + url)
+                    if response.status_code == 200:
+                        break
+            
+            if response.status_code == 302:
+                print(f"Failed to fetch {url}")
+                time.sleep(5)
+                continue
+
             soup = BeautifulSoup(response.text, "html.parser")
-            title = soup.find(attrs={"id":"ctl00_content_bookDetails_dTitle"}).text
+            try:
+                title = soup.find(attrs={"id":"ctl00_content_bookDetails_dTitle"}).text
+            except Exception as e:
+                print(e)
             author = soup.find(attrs={"id":"ctl00_content_bookDetails_dAuthor"}).text
             book_number = soup.find(attrs={"id":"ctl00_content_bookDetails_rptDetails_ctl00_spValue"}).text
-            word_count = soup.find(attrs={"id":"ctl00_content_rptScore_ctl02_valueBox_dValue"}).get("text")
+            word_count = soup.find(attrs={"id":"ctl00_content_rptScore_ctl02_valueBox_dValue"}).text
+            if word_count:
+                word_count = word_count("text")
+
             all_books.append({"title": title, "author": author, "book_number": book_number, "word_count": word_count})
             
         return all_books
@@ -123,6 +150,13 @@ def main(username, password, school_id):
     ar.connect(school_id=school_id, username=username, password=password)
     all_books = ar.get_all_books(school_id=school_id)
     pprint.pprint(all_books)
+
+    temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w+t')
+    print(f"Saving to {temp_file.name}")
+    temp_file.writelines(json.dumps(all_books))
+    temp_file.close()
+
+
 
 
 if __name__ == "__main__":
