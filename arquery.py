891fd131f342ee56046902f8d65e61205a28f00d
Greg Ford <greg.ford.2@googlemail.com>
2022-12-04 09:37:59 +1000

    Various updates

--- arquery.py
+++ arquery.py
@@ -9,6 +9,8 @@ import pprint
 import time
 import tempfile
 import json
+import re
+from loguru import logger
 
 requests.packages.urllib3.disable_warnings()
 
@@ -17,10 +19,10 @@ class arquery:
     def __init__(self) -> None:
         self.session = requests.session()
 
-    def connect(self, school_id, username, password):
+    def connect(self, base_url, school_id, username, password, verify=True):
         response = self.session.get(
-            url=f"https://auhosted4.renlearn.com.au/{school_id}/Public/RPM/Login/Login.aspx?srcID=s",
-            verify=False,
+            url=f"{base_url}/{school_id}/Public/RPM/Login/Login.aspx?srcID=s",
+            verify=verify,
         )
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -43,21 +45,20 @@ class arquery:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         self.session.post(
-            url=f"https://auhosted4.renlearn.com.au/{school_id}/Public/RPM/Login/Login.aspx?srcID=s",
+            url=f"{base_url}/{school_id}/Public/RPM/Login/Login.aspx?srcID=s",
             data=payload,
-            headers=headers,
-            verify=False,
+            headers=headers
         )
 
-        print(self.session.cookies)
+        logger.debug(self.session.cookies)
 
 
-    def get_all_books(self, school_id):
+    def get_all_books(self, base_url, school_id):
         all_books = []
 
         # Get the first page of books
         response = self.session.get(
-            f"https://auhosted4.renlearn.com.au/{school_id}/AR/StudentApp/Bookshelf.aspx"
+            f"{base_url}/{school_id}/AR/StudentApp/Bookshelf.aspx"
         )
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -76,7 +77,7 @@ class arquery:
 
         # Get all subsequent pages (up to 100)
         for page in range(0,100):
-            print(f"Page: {page}")
+            logger.info(f"Processing page: {page}")
             payload = (
                 f"__LASTFOCUS=&__EVENTTARGET=ctl00%24content%24pnSearchHeader%24mLinkButton_Next&__EVENTARGUMENT="
                 f"&__VIEWSTATE={viewstate}&__VIEWSTATEGENERATOR={viewstategenerator}"
@@ -87,7 +88,7 @@ class arquery:
             headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
             response = self.session.post(
-                url=f"https://auhosted4.renlearn.com.au/{school_id}/AR/StudentApp/Bookshelf.aspx",
+                url=f"{base_url}/{school_id}/AR/StudentApp/Bookshelf.aspx",
                 data=payload,
                 headers=headers,
                 verify=False,
@@ -97,30 +98,30 @@ class arquery:
             book_ids = [id.attrs['data-id'] for id in soup.find_all(attrs={"id":"dBook"})]
             all_book_ids.extend(book_ids)
 
-            # Exit when we're on the last page
+            # Exit when we're on the last page  
             if soup.find('a', attrs={"id": "ctl00_content_pnSearchHeader_mLinkButton_Next"}).get("disabled") == "disabled":
                 break
         
-        print(len(all_book_ids))
+        logger.info(f"Total book id count: {len(all_book_ids)}")
 
         b = 0
         for book_id in all_book_ids:
-            print(f"Retrieving book {b} - i = {book_id}")
-            url = url=f"https://auhosted4.renlearn.com.au/{school_id}/AR/StudentApp/BookScore.aspx?i=" + book_id
+            logger.info(f"Retrieving book {b} - i = {book_id}")
+            url = url=f"{base_url}/{school_id}/AR/StudentApp/BookScore.aspx?i=" + book_id
             response = self.session.get(url=url, verify=False, allow_redirects=False)
-            print(str(response.status_code) + " - " + str(len(response.text)) + " - " + url)
+            logger.debug(str(response.status_code) + " - " + str(len(response.text)) + " - " + url)
             
             # retry up to 5 times, with 5 second pauses
             if response.status_code == 302:
                 for i in (1,5):
                     time.sleep(5)
                     response = self.session.get(url=url, verify=False, allow_redirects=False)
-                    print(str(response.status_code) + " - " + str(len(response.text)) + " - " + url)
+                    logger.debug(str(response.status_code) + " - " + str(len(response.text)) + " - " + url)
                     if response.status_code == 200:
                         break
             
             if response.status_code == 302:
-                print(f"Failed to fetch {url}")
+                logger.error(f"Failed to fetch {url}")
                 time.sleep(5)
                 continue
 
@@ -129,13 +130,16 @@ class arquery:
                 title = soup.find(attrs={"id":"ctl00_content_bookDetails_dTitle"}).text
             except Exception as e:
                 # This shouldn't happen - we got a response but no reference to the book title
-                print(e)
+                logger.error(e)
 
-            author = soup.find(attrs={"id":"ctl00_content_bookDetails_dAuthor"}).text
+            author = soup.find(attrs={"id":"ctl00_content_bookDetails_dAuthor"}).text.replace("by ","")
             book_number = soup.find(attrs={"id":"ctl00_content_bookDetails_rptDetails_ctl00_spValue"}).text
-            word_count = soup.find(attrs={"id":"ctl00_content_rptScore_ctl02_valueBox_dValue"}).text
+            # word_count = soup.find(attrs={"id":re.compile("ctl0._content_rptScore_ctl0._valueBox_dValue")})
+            word_count = soup.find(attrs={"id":"ctl00_content_rptScore_ctl02_valueBox_dValue"})
             if word_count:
-                word_count = word_count("text")
+                word_count = word_count.text.replace(",","").replace(" EN","")
+            else:
+                logger.info(f"No word count for book id {id} - student may not have passed the quiz")
 
             all_books.append({"title": title, "author": author, "book_number": book_number, "word_count": word_count})
             b += 1
@@ -143,20 +147,16 @@ class arquery:
         return all_books
 
 
-def main(username, password, school_id):
-    url = f"https://auhosted4.renlearn.com.au/{school_id}/Public/RPM/Login/Login.aspx?srcID=s"
-
+def main(base_url, username, password, school_id):
     ar = arquery()
-    ar.connect(school_id=school_id, username=username, password=password)
-    all_books = ar.get_all_books(school_id=school_id)
+    ar.connect(base_url=base_url, school_id=school_id, username=username, password=password, verify=False)
+    all_books = ar.get_all_books(base_url=base_url, school_id=school_id)
     pprint.pprint(all_books)
 
     temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w+t')
-    print(f"Saving to {temp_file.name}")
+    logger.info(f"Saving to {temp_file.name}")
     temp_file.writelines(json.dumps(all_books))
     temp_file.close()
-
-
 
 
 if __name__ == "__main__":
@@ -164,12 +164,13 @@ if __name__ == "__main__":
     parser.add_argument('-u', '--username', required=True, type=str)
     parser.add_argument('-p', '--password', required=False, type=str)
     parser.add_argument('-s', '--schoolid', required=True, type=str)
+    parser.add_argument('-w', '--website', required=True, type=str)
 
     args = parser.parse_args()
 
     if not args.password: 
         args.password = getpass()
 
-    main(username=args.username, password=args.password, school_id=args.schoolid)
+    main(base_url=args.website, username=args.username, password=args.password, school_id=args.schoolid)
 
 
